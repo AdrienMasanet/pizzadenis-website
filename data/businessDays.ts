469b6d93900523246f757f7a1a3bdc5c574cf6fc
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-03 22:46:32 +0100

    feat: add businessDays data

---
+++ data/businessDays.ts
@@ -0,0 +1,13 @@
+import BusinessDay from "@/interfaces/BusinessDay";
+
+const businessDays: BusinessDay[] = [
+  { name: "dimanche", startHour: 18.0, endHour: 21.0 },
+  { name: "lundi", startHour: 0.0, endHour: 0.0, dayOff: true },
+  { name: "mardi", startHour: 18.0, endHour: 21.0 },
+  { name: "mercredi", startHour: 18.0, endHour: 21.0 },
+  { name: "jeudi", startHour: 18.0, endHour: 21.0 },
+  { name: "vendredi", startHour: 18.0, endHour: 21.0 },
+  { name: "samedi", startHour: 18.0, endHour: 21.0 },
+];
+
+export default businessDays;
