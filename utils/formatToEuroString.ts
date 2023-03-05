24d67f3d35745c83766e17a450728ee9914f854b
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-06 00:43:21 +0100

    feat: add formatToEuroString utility function

---
+++ utils/formatToEuroString.ts
@@ -0,0 +1,8 @@
+/**
+ * Format a number to a string with euro currency
+ * @param price The price to format
+ * @returns {string} The formatted price
+ */
+export default function formatToEuroString(price: number): string {
+  return price.toLocaleString("fr-FR", { style: "currency", currency: "EUR" }).toString();
+}
