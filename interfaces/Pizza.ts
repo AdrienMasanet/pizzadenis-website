86b2be20da527466ce79db4968db29e3cbd22d54
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-05 23:11:19 +0100

    feat: add Pizza interface

---
+++ interfaces/Pizza.ts
@@ -0,0 +1,12 @@
+import Ingredient from "@/interfaces/Ingredient";
+import PizzaCategory from "@/interfaces/PizzaBase";
+
+interface Pizza {
+  name: string;
+  priceSmall: number;
+  priceBig?: number;
+  ingredients: Ingredient[];
+  base: PizzaCategory;
+}
+
+export default Pizza;
