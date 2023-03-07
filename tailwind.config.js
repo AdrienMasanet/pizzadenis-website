b021d2c466567ad85fb44dc2dd03a912d09f80b7
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-07 23:53:22 +0100

    feat: GT Walsheim Pro as main sans font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
+      sans: ["GT Walsheim Pro", "sans-serif"],
       babyaletha: ["Baby Aletha", "serif"],
       roastchicken: ["Roast Chicken", "sans-serif"],
     },
