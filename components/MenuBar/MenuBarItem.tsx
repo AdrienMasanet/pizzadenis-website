7336f269a820ba207932f68a22d9d95a85f9e83e
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-08 00:13:10 +0100

    feat: add MenuBarItem component

---
+++ components/MenuBar/MenuBarItem.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+type MenuBarItemProps = {
+  text: string;
+  link: string;
+};
+
+export const MenuBarItem = ({ text, link }: MenuBarItemProps) => {
+  return (
+    <Link className="list-none px-3 py-1 rounded hoverable text-xl font-light" href={link}>
+      <p>{text}</p>
+    </Link>
+  );
+};
+
+export default MenuBarItem;
