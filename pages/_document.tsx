fc578af319f67f3d75f9d653f8b543c4fe4f7078
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-08 00:15:18 +0100

    feat: MenuBar and Footer components on all pages and change language to fr

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import MenuBar from "@/components/MenuBar/MenuBar";
+import Footer from "@/components/Footer/Footer";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="fr">
       <Head />
       <body>
+        <MenuBar />
         <Main />
         <NextScript />
       </body>
+      <Footer />
     </Html>
-  )
+  );
 }
