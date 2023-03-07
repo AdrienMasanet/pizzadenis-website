30903c596c52614e29ec10146420baacc9ea058b
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-07 23:57:04 +0100

    feat: add CurrentlyOpen component

---
+++ components/CurrentlyOpen/CurrentlyOpen.tsx
@@ -0,0 +1,13 @@
+import useCurrentlyOpen from "@/hooks/useCurrentlyOpen";
+
+const CurrentlyOpen = ({}) => {
+  const [isOpen, message] = useCurrentlyOpen();
+
+  return (
+    <div className={`${isOpen ? "neon-borders" : ""} w-fit px-3 py-2 mx-auto my-10 rounded-lg bg-stone-50/60 border border-black/20 backdrop-blur-sm shadow-lg`}>
+      <p>{message}</p>
+    </div>
+  );
+};
+
+export default CurrentlyOpen;
