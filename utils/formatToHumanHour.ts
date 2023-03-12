875e7e8b277f7fe18e4c11812be217936fc18d5b
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-12 20:10:27 +0100

    feat: add formatToHumanHour utility function

---
+++ utils/formatToHumanHour.ts
@@ -0,0 +1,12 @@
+/**
+ * Format a number representing an hour to a human-readable string with the format "HH:mm"
+ * For instance, 9.3 will be formatted to "09:30", 12.45 will be formatted to "12:45", 18.0 will be formatted to "18:00", 19.01 will be formatted to "19:01" and so on.
+ * @param hour The hour to format
+ * @returns {string} The formatted human-readable hour
+ */
+export default function formatToHumanHour(hour: number) {
+  const hours: number = Math.floor(hour);
+  const minutes: number = Math.round((hour - hours) * 100);
+
+  return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+}
