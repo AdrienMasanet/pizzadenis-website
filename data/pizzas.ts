7c8f4c0ca976d1747298961ebf972187a1718644
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-28 19:49:29 +0200

    fix: removed double entry for végétarienne

--- data/pizzas.ts
+++ data/pizzas.ts
@@ -38,7 +38,6 @@ const pizzas: Pizza[] = [
   { name: "Zoubida", priceSmall: 9.0, priceBig: 12.0, ingredients: [ingredients.tomato, ingredients.merguez, ingredients.cheese], base: pizzaBases.tomatoBase },
   { name: "Lila", priceSmall: 10.0, priceBig: 13.0, ingredients: [ingredients.tomato, ingredients.onions, ingredients.merguez, ingredients.cheese], base: pizzaBases.tomatoBase },
   { name: "L'orientale", priceSmall: 10.5, priceBig: 13.5, ingredients: [ingredients.tomato, ingredients.onions, ingredients.pepper, ingredients.merguez, ingredients.cheese], base: pizzaBases.tomatoBase },
-  { name: "Végétarienne", priceSmall: 10.5, priceBig: 13.5, ingredients: [ingredients.tomato, ingredients.mushrooms, ingredients.pepper, ingredients.artichokes, ingredients.cheese], base: pizzaBases.tomatoBase },
   { name: "Angela", priceSmall: 10.5, priceBig: 13.5, ingredients: [ingredients.tomato, ingredients.eggplants, ingredients.ham, ingredients.cheese], base: pizzaBases.tomatoBase },
   { name: "Palerma", priceSmall: 12.0, priceBig: 15.0, ingredients: [ingredients.tomato, ingredients.eggplants, ingredients.ham, ingredients.mushrooms, ingredients.chorizo, ingredients.cheese], base: pizzaBases.tomatoBase },
   { name: "Chef", priceSmall: 13.0, priceBig: 16.0, ingredients: [ingredients.tomato, ingredients.eggplants, ingredients.ham, ingredients.mushrooms, ingredients.chorizo, ingredients.artichokes, ingredients.cheese, ingredients.olives], base: pizzaBases.tomatoBase },
@@ -50,7 +49,7 @@ const pizzas: Pizza[] = [
   { name: "Stromboli", priceSmall: 13.0, priceBig: 16.0, ingredients: [ingredients.cream, ingredients.roquefort, ingredients.onions, ingredients.mushrooms, ingredients.mincedMeat, ingredients.cheese], base: pizzaBases.creamBase },
   { name: "St Jacques", priceSmall: 12.5, priceBig: 15.5, ingredients: [ingredients.cream, ingredients.cheese, ingredients.freshScallopsWithCoral, ingredients.garlic, ingredients.parsley, ingredients.lemon], base: pizzaBases.creamBase },
   { name: "Lydie", priceSmall: 10.0, priceBig: 13.0, ingredients: [ingredients.tomato, ingredients.mushrooms, ingredients.pepper, ingredients.cheese], base: pizzaBases.tomatoBase },
-  { name: "Vegetarienne", priceSmall: 10.5, priceBig: 13.5, ingredients: [ingredients.tomato, ingredients.mushrooms, ingredients.artichokes, ingredients.pepper, ingredients.cheese], base: pizzaBases.tomatoBase },
+  { name: "Végétarienne", priceSmall: 10.5, priceBig: 13.5, ingredients: [ingredients.tomato, ingredients.mushrooms, ingredients.artichokes, ingredients.pepper, ingredients.cheese], base: pizzaBases.tomatoBase },
   { name: "Paysanne", priceSmall: 10.0, priceBig: 13.0, ingredients: [ingredients.tomato, ingredients.onions, ingredients.baconBits, ingredients.cheese], base: pizzaBases.tomatoBase },
 ];
 
