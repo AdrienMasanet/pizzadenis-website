33481f3e455eed689b8b19e31b024f75de23cd93
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-28 19:50:51 +0200

    feat: add SocialMediaIcon component

---
+++ components/SocialMediaIcon/SocialMediaIcon.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+type SocialMediaIconProps = {
+  name: string;
+  link: string;
+  image: string;
+};
+
+const SocialMediaIcon = ({ name, link, image }: SocialMediaIconProps) => {
+  return (
+    <a href={link} target="_blank" rel="noreferrer">
+      <Image src={image} alt={`Pizza Denis sur ${name}`} width={20} height={25} />
+    </a>
+  );
+};
+
+export default SocialMediaIcon;
