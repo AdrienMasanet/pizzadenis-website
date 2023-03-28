a06f4aba907a582817a5a71142ce1097eaad4234
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-28 16:56:55 +0200

    feat: TripAdvisorLabel component

---
+++ components/TripAdvisorLabel/TripAdvisorLabel.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+const TripAdvisorLabel = () => {
+  return (
+    <div id="TA_excellent685" className="TA_excellent">
+      <ul id="P1pstkZ977" className="TA_links abb0cQR">
+        <li id="I1BfX0" className="Z1ytrMo">
+          <a target="_blank" rel="noreferrer" href="https://www.tripadvisor.fr/Restaurant_Review-g2482779-d5965360-Reviews-Pizza_Denis-Saint_Laurent_de_la_Salanque_Pyrenees_Orientales_Occitanie.html">
+            <Image src="https://static.tacdn.com/img2/brand_refresh/Tripadvisor_lockup_horizontal_secondary_registered.svg" alt="TripAdvisor" width={150} height={50} />
+          </a>
+        </li>
+      </ul>
+      <script async src="https://www.jscache.com/wejs?wtype=excellent&amp;uniq=685&amp;locationId=5965360&amp;lang=fr&amp;display_version=2" data-loadtrk></script>
+    </div>
+  );
+};
+
+export default TripAdvisorLabel;
