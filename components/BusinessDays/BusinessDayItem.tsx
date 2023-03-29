25497b120f3be3b7df0e3a0838898dbd9701d3c7
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-29 21:10:44 +0200

    refactor: handle optional startHour and endHour

--- components/BusinessDays/BusinessDayItem.tsx
+++ components/BusinessDays/BusinessDayItem.tsx
@@ -12,16 +12,16 @@ const BusinessDayItem = ({ businessDay }: BusinessDayItemProps) => {
         <td className="capitalize py-1 px-2">
           <span className="bg-black text-white font-bold rounded p-1 mr-5">{businessDay.name}</span>
         </td>
-        {businessDay.dayOff ? (
-          <td className="text-center text-red-500" colSpan={3}>
-            Fermé
-          </td>
-        ) : (
+        {!businessDay.dayOff && businessDay.startHour && businessDay.endHour ? (
           <>
             <td className="text-center">{formatToHumanHour(businessDay.startHour)}</td>
             <td className="px-2 mx-2">|</td>
             <td className="text-center">{formatToHumanHour(businessDay.endHour)}</td>
           </>
+        ) : (
+          <td className="text-center text-red-500" colSpan={3}>
+            Fermé
+          </td>
         )}
       </tr>
     </tbody>
