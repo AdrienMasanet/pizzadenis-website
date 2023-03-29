a85dea32bc4697d4271684fa20ecde25d0873add
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-29 14:58:38 +0200

    chore: add coverage and module aliases

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,18 +1,24 @@
-// jest.config.mjs
 import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
 // Add any custom config to be passed to Jest
 /** @type {import('jest').Config} */
 const config = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-
+  moduleNameMapper: {
+    "^@/mocks/(.*)$": "<rootDir>/__mocks__/$1",
+    "^@/components/(.*)$": "<rootDir>/components/$1",
+    "^@/context/(.*)$": "<rootDir>/context/$1",
+    "^@/hooks/(.*)$": "<rootDir>/hooks/$1",
+    "^@/pages/(.*)$": "<rootDir>/pages/$1",
+    "^@/services/(.*)$": "<rootDir>/services/$1",
+    "^@/utils/(.*)$": "<rootDir>/utils/$1",
+  },
   testEnvironment: "jest-environment-jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: ["components/**/*.{js,jsx,ts,tsx}", "context/**/*.{js,jsx,ts,tsx}", "hooks/**/*.{js,jsx,ts,tsx}", "pages/**/*.{js,jsx,ts,tsx}", "services/**/*.{js,jsx,ts,tsx}", "utils/**/*.{js,jsx,ts,tsx}"],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
