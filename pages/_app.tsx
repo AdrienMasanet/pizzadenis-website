ef8a9aab3d55c348616f432cc48d45b802696eb3
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-03-29 17:15:22 +0200

    fix: correct wrong use of _document.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import MenuBar from "@/components/MenuBar/MenuBar";
+import Footer from "@/components/Footer/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,7 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
+      <MenuBar />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import MenuBar from "@/components/MenuBar/MenuBar";
-import Footer from "@/components/Footer/Footer";
 
 export default function Document() {
   return (
     <Html lang="fr">
       <Head />
       <body>
-        <MenuBar />
         <Main />
         <NextScript />
       </body>
-      <Footer />
     </Html>
   );
 }
