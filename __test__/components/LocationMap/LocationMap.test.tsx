1e0792a2a4eb087d7c9f8e3ba389d0bda6620c4f
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-03 22:43:01 +0200

    test: add tests for LocationMap component

---
+++ __test__/components/LocationMap/LocationMap.test.tsx
@@ -0,0 +1,33 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import LocationMap from "@/components/LocationMap/LocationMap";
+
+// Mock the Mapbox library
+jest.mock("mapbox-gl", () => ({
+  Map: jest.fn(() => ({
+    on: jest.fn(),
+    remove: jest.fn(),
+  })),
+}));
+
+describe("component | LocationMap", () => {
+  beforeEach(() => {
+    render(<LocationMap />);
+  });
+
+  it("should render marker which means the map is loaded", () => {
+    const marker = screen.getByAltText("Marqueur de carte avec le logo de Pizza Denis");
+    expect(marker).toBeInTheDocument();
+  });
+
+  it("should open Google Maps link for Pizza Denis in a new tab when clicking on the marker", () => {
+    const windowOpenSpy = jest.spyOn(window, "open").mockImplementation(() => null);
+    const marker = screen.getByAltText("Marqueur de carte avec le logo de Pizza Denis");
+
+    fireEvent.click(marker);
+
+    expect(windowOpenSpy).toHaveBeenCalledWith(process.env.NEXT_PUBLIC_GOOGLE_MAPS_LINK, "_blank");
+
+    windowOpenSpy.mockRestore();
+  });
+});
