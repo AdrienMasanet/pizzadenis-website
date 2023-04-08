6da3540fe6415df1b30c66e7a30447f020b23b30
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-08 17:52:20 +0200

    fix: add playlist id for autoplay

--- components/MainSlider/MainSlider.tsx
+++ components/MainSlider/MainSlider.tsx
@@ -52,7 +52,7 @@ const MainSlider = () => {
           <Image src={sliderImage1} alt="Slider image 1" fill className="object-cover" />
         </div>
         <div className="keen-slider__slide slider-video">
-          <iframe id="ytplayer" width="720" height="405" src="https://www.youtube.com/embed/0MN8ZlGhBoM?autoplay=1&controls=0&disablekb=1&fs=0&loop=1&modestbranding=1&playsinline=1&iv_load_policy=3&start=33&mute=1" allow="autoplay" />
+          <iframe id="ytplayer" width="720" height="405" src="https://www.youtube.com/embed/0MN8ZlGhBoM?autoplay=1&controls=0&disablekb=1&fs=0&loop=1&modestbranding=1&playsinline=1&iv_load_policy=3&start=33&mute=1&playlist=0MN8ZlGhBoM" allow="autoplay" />
         </div>
         <div className="keen-slider__slide">
           <Image src={sliderImage2} alt="Slider image 2" fill className="object-cover" />
