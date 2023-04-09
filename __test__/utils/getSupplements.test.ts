7658a47a02cba873d1999948a913242a506416b5
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-09 20:15:46 +0200

    test: add tests for getSupplements

---
+++ __test__/utils/getSupplements.test.ts
@@ -0,0 +1,43 @@
+import getSupplements from "@/utils/getSupplements";
+import * as allIngredients from "@/data/ingredients";
+import Ingredient from "@/interfaces/Ingredient";
+
+// Mock the ingredients data
+jest.mock("@/data/ingredients", () => {
+  return {
+    tomato: { name: "Tomate", priceAsSupplement: 1 },
+    cheese: { name: "Fromage", isOptional: true, priceAsSupplement: 1 },
+    chicken: { name: "Poulet", isMeat: true, priceAsSupplement: 1.5 },
+    tuna: { name: "Thon", isSeaFood: true, priceAsSupplement: 1.5 },
+    salmon: { name: "Saumon", isSeaFood: true, priceAsSupplement: null },
+  };
+});
+
+describe("utils | getSupplements", () => {
+  it("should return an object", () => {
+    expect(typeof getSupplements()).toBe("object");
+  });
+
+  it("should return the correct supplements object structure", () => {
+    const supplements = getSupplements();
+    const expectedSupplements = {
+      "1": [
+        { name: "Tomate", priceAsSupplement: 1 },
+        { name: "Fromage", isOptional: true, priceAsSupplement: 1 },
+      ],
+      "1.5": [
+        { name: "Poulet", isMeat: true, priceAsSupplement: 1.5 },
+        { name: "Thon", isSeaFood: true, priceAsSupplement: 1.5 },
+      ],
+    };
+
+    expect(supplements).toEqual(expectedSupplements);
+  });
+
+  it("should exclude ingredients without a priceAsSupplement", () => {
+    const supplements = getSupplements();
+    const hasSalmon = supplements["1.5"].some((ingredient: Ingredient) => ingredient.name === "Saumon");
+
+    expect(hasSalmon).toBe(false);
+  });
+});
