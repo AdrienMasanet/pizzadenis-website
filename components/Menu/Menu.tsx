cf1cc3ab0385b4de5861a9c20ac1767ca7aca280
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-04-24 12:55:47 +0200

    feat: handle only one pizza result text variation

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -6,9 +6,19 @@ type MenuProps = {
 };
 
 const Menu = ({ pizzas }: MenuProps) => {
+  const getResultText = (pizzasLength: number): string => {
+    if (pizzasLength < 1) {
+      return "Pas de pizza trouvée avec ces filtres de recherche";
+    } else if (pizzasLength === 1) {
+      return pizzasLength + " pizza";
+    } else {
+      return pizzasLength + " pizzas";
+    }
+  };
+
   return (
     <div className="">
-      <p className="text-center text-xs white-outline my-5">{pizzas.length < 1 ? "Pas de pizza trouvée avec ces filtres de recherche" : pizzas.length + " pizzas"}</p>
+      <p className="text-center text-xs white-outline my-5">{getResultText(pizzas.length)}</p>
       <div className="grid grid-flow-row grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5">
         {pizzas.map((pizza, index) => (
           <PizzaMenuItem pizza={pizza} key={index} />
