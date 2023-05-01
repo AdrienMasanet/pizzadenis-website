6d23e1cb504df916d04114fc758aca1dd220f873
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-05-01 13:30:15 +0200

    feat: more vertical space on small screens

--- components/MainSlider/MainSlider.tsx
+++ components/MainSlider/MainSlider.tsx
@@ -35,7 +35,7 @@ const MainSlider = () => {
   );
 
   return (
-    <div className="relative w-full py-20 md:py-36 overflow-hidden">
+    <div className="relative w-full py-28 md:py-36 overflow-hidden">
       <Image src={logoImage} alt="Pizza Denis depuis 1984" width={300} className="p-3 object-cover mx-auto white-outline" />
       <p className="mx-auto p-2 md:mb-20 font-babyaletha text-xl leading-9 text-center w-fit white-outline">
         <span className="px-3 text-4xl rounded-lg rounded-b-none">Pizzas au feu de bois</span>
@@ -48,10 +48,16 @@ const MainSlider = () => {
       </div>
       <div ref={sliderRef} className="keen-slider h-full !absolute top-0 -z-10 opacity-40">
         <div className="keen-slider__slide">
-          <Image src={sliderImage1} alt="Slider image 1" fill className="object-cover" />
+          <Image src={sliderImage1} alt="Slider image 1" fill className="object-cover" priority />
         </div>
         <div className="keen-slider__slide slider-video">
-          <iframe id="ytplayer" width="720" height="405" src="https://www.youtube.com/embed/0MN8ZlGhBoM?autoplay=1&controls=0&disablekb=1&fs=0&loop=1&modestbranding=1&playsinline=1&iv_load_policy=3&start=33&mute=1&playlist=0MN8ZlGhBoM" allow="autoplay" />
+          <iframe
+            id="ytplayer"
+            width="720"
+            height="405"
+            src="https://www.youtube.com/embed/0MN8ZlGhBoM?autoplay=1&controls=0&disablekb=1&fs=0&loop=1&modestbranding=1&playsinline=1&iv_load_policy=3&start=33&mute=1&playlist=0MN8ZlGhBoM"
+            allow="autoplay"
+          />
         </div>
         <div className="keen-slider__slide">
           <Image src={sliderImage2} alt="Slider image 2" fill className="object-cover" />
