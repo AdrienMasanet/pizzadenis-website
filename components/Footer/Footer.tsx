8b453ee483a3d6aa8693d6b44ee4f0654593343a
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-05-01 17:44:26 +0200

    feat: add more link space for small screen sizes

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -12,19 +12,19 @@ const Footer = () => {
     <footer className="w-full bg-stone-900 text-white border-t-4 border-white/85 border-double" id="footer">
       <div className="max-w-screen-md mx-auto p-4 flex flex-col md:flex-row justify-evenly items-center md:gap-10">
         <nav className="order-2 md:order-1 flex-1 flex flex-col items-center md:items-end">
-          <Link className="w-fit hoverable white" href="/">
+          <Link className="w-fit h-12 md:h-auto hoverable white" href="/">
             Accueil
           </Link>
-          <Link className="w-fit hoverable white" href="la-carte">
+          <Link className="w-fit h-12 md:h-auto hoverable white" href="la-carte">
             La carte des pizzas
           </Link>
         </nav>
         <Image src={logoImage} alt="Pizza Denis depuis 1984" height={150} className="order-1 md:order-2 flex-1 my-10 invert" />
         <nav className="order-3 md:order-3 flex-1 flex flex-col items-center md:items-start mb-10 md:mb-0">
-          <Link className="w-fit hoverable white" href="equipe">
+          <Link className="w-fit h-12 md:h-auto hoverable white" href="equipe">
             L&apos;équipe
           </Link>
-          <Link className="w-fit hoverable white" href="contact">
+          <Link className="w-fit h-12 md:h-auto hoverable white" href="contact">
             Nous contacter
           </Link>
         </nav>
