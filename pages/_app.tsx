0f8b59216e79e5d716c68cfea52d4657bd276efd
AdrienMasanet <adrien.masanet@hotmail.fr>
2023-05-01 13:40:58 +0200

    chore: add Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import MenuBar from "@/components/MenuBar/MenuBar";
 import Footer from "@/components/Footer/Footer";
 import { PizzaListProvider } from "@/context/PizzaListContext";
 import { ModalProvider } from "@/context/ModalContext";
+import { Analytics } from "@vercel/analytics/react";
 import logoImage from "@/public/images/logo.png";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -29,6 +30,7 @@ export default function App({ Component, pageProps }: AppProps) {
         </ModalProvider>
       </PizzaListProvider>
       <Footer />
+      <Analytics />
     </>
   );
 }
